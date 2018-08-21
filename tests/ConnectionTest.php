b5773bb174b9daae01545ae4fdcdbcf5fab5f2de
Felipe Abreu <coisa.cs@gmail.com>
2018-08-21 15:28:00 -0300

    Adiciona testes para a classe de conexão

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the unipago/api-sdk-php
  *
@@ -9,21 +10,96 @@
  * @link https://packagist.org/packages/unipago/api-sdk-php Packagist
  * @link https://github.com/kinghost/UniPago-SDK-PHP GitHub
  */
+
 namespace UnipagoApi\Test;
 
+use Dotenv\Dotenv;
 use PHPUnit\Framework\TestCase;
+use UnipagoApi\Connection;
 
 /**
  * Class ConnectionTest
+ *
  * @package UnipagoApi\Test
  */
 class ConnectionTest extends TestCase
 {
     /**
-     * Test class_exists
+     * @expectedException \UnipagoApi\Exception\UnipagoException
      */
-    public function testDummy()
+    public function testInvalidScopeThrowsException()
     {
-        $this->assertTrue(class_exists('UnipagoApi\\Connection', false));
+        new Connection(uniqid(), uniqid(), uniqid());
+    }
+
+    /**
+     * @expectedException \UnipagoApi\Exception\UnipagoException
+     */
+    public function testInvalidClientCredentialsThrowsException()
+    {
+        new Connection(Connection::SCOPE_SANDBOX, uniqid(), uniqid());
+    }
+
+    /**
+     * Carrega clientId e clientSecret do arquivo .env caso já não tenha sido definido
+     *
+     * @return array
+     */
+    private function getClientCredentials()
+    {
+        if (!getenv('CLIENT_ID') || !getenv('CLIENT_SECRET')) {
+            $dotenv = new Dotenv(dirname(__DIR__));
+            $dotenv->load();
+        }
+
+        return [
+            getenv('CLIENT_ID'),
+            getenv('CLIENT_SECRET')
+        ];
+    }
+
+    public function testValidClientCredentialsGenerateAccessToken()
+    {
+        list($clientId, $clientSecret) = $this->getClientCredentials();
+
+        $connection = new Connection(Connection::SCOPE_SANDBOX, $clientId, $clientSecret);
+
+        $this->assertInternalType('string', $connection->accessToken);
+        $this->assertRegExp('/^.*\..*\..*$/', $connection->accessToken);
+
+        return $connection;
+    }
+
+    /**
+     * @param Connection $connection
+     *
+     * @depends testValidClientCredentialsGenerateAccessToken
+     */
+    public function testInvalidMethodReturnsBadRequest(Connection $connection)
+    {
+        $response = $connection->send(uniqid(), uniqid());
+
+        $this->assertEquals(400, $response->getStatusCode());
+    }
+
+    /**
+     * @param Connection $connection
+     *
+     * @depends testValidClientCredentialsGenerateAccessToken
+     */
+    public function testInvalidUrlReturnsNotFoundResponse(Connection $connection)
+    {
+        $httpMethods = [
+            Connection::GET,
+            Connection::POST,
+            Connection::PUT,
+            Connection::DELETE
+        ];
+
+        foreach ($httpMethods as $method) {
+            $response = $connection->send($method, uniqid());
+
+            $this->assertEquals(404, $response->getStatusCode());
+        }
     }
 }
